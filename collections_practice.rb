29a13d448ba6a9875feae42beb31ac1552acf781
global-listener-7039 <jodiesternal@yahoo.com>
2020-09-18 16:33:18 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -40,3 +40,13 @@ def sum_array(array)
     sum+=num
   end
   sum
+
+  def add_s(array)
+  array.collect do |word|
+    if array[1] == word
+      word
+    else
+      word + "s"
+    end
+  end
+end 
